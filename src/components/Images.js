d1699764c53415aadd5c654ba8f07ab0be63fd05
dcuevasa <d.cuevas@uniandes.edu.co>
2024-09-26 10:26:13 -0500

    algo2

--- src/components/Images.js
+++ src/components/Images.js
@@ -3,31 +3,52 @@ import './Images.css';
 
 function Images() {
 
-    const [imageUrls, setImageUrls] = useState(["https://picsum.photos/350","https://picsum.photos/350","https://picsum.photos/350","https://picsum.photos/350","https://picsum.photos/350","https://picsum.photos/350","https://picsum.photos/350","https://picsum.photos/350","https://picsum.photos/350","https://picsum.photos/350","https://picsum.photos/350","https://picsum.photos/350"]);
-
-    useEffect(() => {
-        const fetchImages = async () => {
-            const urls = [];
-            for (let i = 0; i < 12; i++) {
-                try {
-                    const response = await fetch(`https://picsum.photos/350`);
-                    urls.push(response.url);
-                } catch (error) {
-                    console.error('Error fetching image:', error);
-                }
-            }
-            setImageUrls(urls);
-        };
-
-        fetchImages();
-    }, []);
-
     return (
-      <div id="Images">
-        {imageUrls.map((url, index) => (
-          <img src={url} alt={`Random ${index}`} key={index} className="images" />
-        ))}
-      </div>
+          <div id="Images">
+            <div id="Titulos">
+            <h1>Cycling</h1>
+            <h1>Running</h1>
+            <h1>Swiming</h1>
+            </div>
+            <div id="Tarjetas">
+                <div className="Columnas">
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                </div>
+                <div className="Columnas">
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                </div>
+                <div className="Columnas">
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                      <img src="https://picsum.photos/350" className="images" />
+                </div>
+            </div>
+          </div>
     );
 }
 
